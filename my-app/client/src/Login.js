d6b6da26749fde70421f1934cd306b09fee0d83c
Karan <karanwarraich@outlook.com>
2023-07-15 12:58:44 -0600

    new

--- my-app/client/src/Login.js
+++ my-app/client/src/Login.js
@@ -51,7 +51,7 @@ export default function SignIn() {
     const _email = data.get("email");
     const _password = data.get("password");
     axios
-      .post("https:https://test-9o0j.onrender.com/auth/login", {
+      .post("https://test-9o0j.onrender.com/auth/login", {
         email: _email,
         password: _password,
       })
