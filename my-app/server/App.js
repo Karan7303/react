9965b6b6cbc930f65574a81a63a4f05edae52948
Karan <karanwarraich@outlook.com>
2023-07-15 12:23:05 -0600

    temp

--- my-app/server/App.js
+++ my-app/server/App.js
@@ -28,6 +28,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
 app.use(express.static(path.join(__dirnamee,"build")))
 app.use("/assets/", express.static(path.join(__dirnamee, "public/assets/")));
 
