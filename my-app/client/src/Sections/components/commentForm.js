f488f98c2aac6d2fc15bc6e3eeb5e8254c428381
Karan <karanwarraich@outlook.com>
2023-07-19 19:34:13 -0600

    added comment path to server

---
+++ my-app/client/src/Sections/components/commentForm.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { TextField,Button } from "@mui/material";
+import { useSelector } from "react-redux";
+import axios from "axios";
+
+function CommentForm(post_id){
+  const token = useSelector((state) => state.token);
+
+  const updateComment=(id)=>{
+    console.log(id);
+    axios
+      .patch(
+        "https://backend-z03p.onrender.com/post/comment/"+id,
+        // {
+        //   params: { postID: p_id },
+        // },
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response);
+        
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+  };
+    return (<form>
+        <TextField
+          style={{ marginLeft:"46px",width:"550px",margin: "15px" }}
+          type="text"
+          label="Add a Comment"
+          variant="outlined"
+          id="commentText"
+        />
+        <br />
+        <Button onClick={()=>updateComment(post_id)} variant="contained" color="primary" style={{ marginLeft:"46px"}}>
+          Comment
+        </Button>
+        </form>
+)}
+export default CommentForm;
