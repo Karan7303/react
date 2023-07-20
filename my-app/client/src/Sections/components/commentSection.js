b9224c5d4fea718ebc5e11144d7f7a3580183b93
Karan <karanwarraich@outlook.com>
2023-07-19 23:24:35 -0600

    changes

--- my-app/client/src/Sections/components/commentSection.js
+++ my-app/client/src/Sections/components/commentSection.js
@@ -1,32 +1,68 @@
-import React from "react";
+import {React,useState} from "react";
 import ReactDOM from "react-dom";
+import useSelector from 'react-redux'
 
-import { Paper,Avatar,Grid } from "@mui/material";
-
+import { Paper, Avatar, Grid } from "@mui/material";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const imgLink =
   "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260";
 
-function CommentSection(props){
+function CommentSection(props) {
+  const token = useSelector((state) => state.token);
+  const [listedUser, setlistedUser] = useState(null);
+  const getUser=(id)=>{
+    axios
+      .get("https://backend-z03p.onrender.com/user/" + id, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then(function (response) {
+        //dispatch(setFriends({friends:response.data[0].friends}))
+        setlistedUser(response.data.userSelect);
+        //console.log(response.data[0].friends);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  var user = props.props;
   return (
     <>
-        <Paper style={{ padding: "15px 15px", marginTop: 10,marginLeft:46 }}>
-        <Grid container wrap="nowrap" spacing={2}>
-          <Grid item>
-            <Avatar alt="Remy Sharp" src={imgLink} />
-          </Grid>
-          <Grid justifyContent="left" item xs zeroMinWidth>
-            <h4 style={{ margin: 0, textAlign: "left" }}>Michel Michel</h4>
-            <p style={{ textAlign: "left" }}>
-              comment #1
-            </p>
-            <p style={{ textAlign: "left", color: "gray" }}>
-              posted 1 minute ago
-            </p>
-          </Grid>
-        </Grid>
-      </Paper>
-      </>
-  )
+      {user.comments.map(function (item, i) {
+        getUser(item.userId)
+        return (
+          <Paper
+            style={{ padding: "15px 15px", marginTop: 10, marginLeft: 46 }}
+          >
+            <Grid container wrap="nowrap" spacing={2}>
+              <Grid item>
+                <Avatar
+                  alt="Remy Sharp"
+                  src={
+                    "https://backend-z03p.onrender.com/assets/" +
+                    listedUser.picture
+                  }
+                />
+              </Grid>
+              <Grid justifyContent="left" item xs zeroMinWidth>
+                <h4 style={{ margin: 0, textAlign: "left" }}>
+                  {" "}
+                  {listedUser.firstName} {listedUser.lastName}
+                </h4>
+                <p style={{ textAlign: "left" }}>{item.}</p>
+                <p style={{ textAlign: "left", color: "gray" }}>
+                  posted 1 minute ago
+                </p>
+              </Grid>
+            </Grid>
+          </Paper>
+        );
+      })}
+    </>
+  );
 }
 export default CommentSection;
